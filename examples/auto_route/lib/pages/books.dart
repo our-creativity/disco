1dc0103c6ce6de205e67df5a37ba0f4fb37f2b17
Alexandru Mariuti <alex@mariuti.com>
2025-01-27 12:52:08 +0100

    chore: auto route example

---
+++ examples/auto_route/lib/pages/books.dart
@@ -0,0 +1,53 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:auto_route_example/blocs/books/bloc.dart';
+import 'package:auto_route_example/router.dart';
+import 'package:disco/disco.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class BooksWrapperPage extends StatelessWidget implements AutoRouteWrapper {
+  const BooksWrapperPage({super.key});
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return ProviderScope(
+      providers: [booksBlocProvider],
+      child: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const AutoRouter();
+  }
+}
+
+@RoutePage()
+class BooksPage extends StatelessWidget {
+  const BooksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final booksBloc = booksBlocProvider.of(context);
+    final books = booksBloc.books;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Books'),
+        leading: BackButton(
+          onPressed: () => const HomeRoute().navigate(context),
+        ),
+      ),
+      body: ListView.separated(
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          final book = books[index];
+          return ListTile(
+            title: Text(book.name),
+            onTap: () => BookRoute(bookId: book.id).navigate(context),
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: 8),
+      ),
+    );
+  }
+}
