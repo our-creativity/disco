ca88a4d5442a9501c13286df34632565d5b2680b
Alexandru Mariuti <alex@mariuti.com>
2025-02-04 10:32:40 +0100

    chore: remove useless private constructor

--- packages/disco/lib/src/models/providers/instantiable_provider.dart
+++ packages/disco/lib/src/models/providers/instantiable_provider.dart
@@ -2,6 +2,5 @@ part of '../../disco_internal.dart';
 
 /// Either a [Provider] or an [InstantiableArgProvider] (i.e. an [ArgProvider]
 /// with its argument).
-sealed class InstantiableProvider {
-  InstantiableProvider._();
-}
+@immutable
+sealed class InstantiableProvider {}
