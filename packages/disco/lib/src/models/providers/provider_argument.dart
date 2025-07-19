b276f32dd463ce4a3e88485bc564a46f0c919098
Manuel Plavsic <55398763+manuel-plavsic@users.noreply.github.com>
2025-07-19 11:19:13 +0000

    chore: revert dispose

--- packages/disco/lib/src/models/providers/provider_argument.dart
+++ packages/disco/lib/src/models/providers/provider_argument.dart
@@ -78,7 +78,7 @@ class ArgProvider<T extends Object, A> {
   /// This method is used internally by [ProviderScope].
   Provider<T> _generateIntermediateProvider(A arg) => Provider<T>(
         (context) => _createValue(context, arg),
-        dispose: (Object value) => _disposeValue?.call(value as T),
+        dispose: _disposeValue,
         lazy: _lazy,
       );
 }
