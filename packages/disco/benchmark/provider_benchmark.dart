88a2f7b120095240ac0437e48cc3e74505c45aad
Alexandru Mariuti <alex@mariuti.com>
2025-12-11 11:07:58 +0100

    chore: remove unused import

--- packages/disco/benchmark/provider_benchmark.dart
+++ packages/disco/benchmark/provider_benchmark.dart
@@ -2,7 +2,6 @@
 
 import 'dart:io';
 import 'package:disco/disco.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
